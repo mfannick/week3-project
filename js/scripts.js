6e3432194a7ada06e5f13dcba2d489b0269708ff
mfannick <mfannick1@gmail.com>
2019-07-26 17:12:08 +0200

    hovering on image in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -10,113 +10,88 @@ $(document).ready(function(){
   /********************************* end of input and submition*************************/
 
   /********************************* hovering on portofolio images***********************/
-//   $('#porto-2-1  #work4').hover(
-//       function(){
-//        $('this').fadeIn()
-//   },function(){
-//     $('this').fadeOut()
-//    $('.overlay-1').hide();
-//     $('.porto-2-1').hover(
-//         function(){   
-//          $('this').stop();
-//          $('.overlay-1').fadeIn("slow");
-//     },function(){
-//       $('this').stop()
-//       $('.overlay-1').fadeOut('slow');
-
-// });
-
-// $('.overlay-2').hide();
-// $('.porto-2-2').hover(
-//     function(){   
-//      $('this').stop();
-//      $('.overlay-2').fadeIn("slow");
-// },function(){
-//   $('this').stop()
-//   $('.overlay-2').fadeOut('slow');
-// });
-
-var overlays=['overlay1','overlay2','overlay3','overlay4','overlay5'
-,'overlay6','overlay7','overlay8']
-var portos=['porto1','porto2','porto3','porto4','porto5','porto6',
-'porto7','porto8']
-
-overlays.forEach(function(overlay){
-    $('.' + overlay).hide();
-})
+  var overlays=['overlay1','overlay2','overlay3','overlay4','overlay5'
+  ,'overlay6','overlay7','overlay8']
+  var portos=['porto1','porto2','porto3','porto4','porto5','porto6',
+  'porto7','porto8']
   
-    $('.porto1').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay1').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay1').fadeOut('slow');
+  overlays.forEach(function(overlay){
+      $('.' + overlay).hide();
+  })
     
-    });
-    $('.porto2').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay2').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay2').fadeOut('slow');
-    
-    });
-    $('.porto3').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay3').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay3').fadeOut('slow');
-    
-    });
-    $('.porto4').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay4').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay4').fadeOut('slow');
-    
-    });
-    $('.porto5').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay5').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay5').fadeOut('slow');
-    
-    });
-    $('.porto6').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay6').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay6').fadeOut('slow');
-    
-    });
-    $('.porto7').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay7').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay7').fadeOut('slow');
-    
-    });
-    $('.porto8').hover(
-        function(){   
-         $('this').stop();
-            $('.overlay8').fadeIn('slow');
-    },function(){
-      $('this').stop()
-        $('.overlay8').fadeOut('slow');
-    
-    });
+      $('.porto1').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay1').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay1').fadeOut('slow');
+      
+      });
+      $('.porto2').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay2').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay2').fadeOut('slow');
+      
+      });
+      $('.porto3').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay3').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay3').fadeOut('slow');
+      
+      });
+      $('.porto4').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay4').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay4').fadeOut('slow');
+      
+      });
+      $('.porto5').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay5').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay5').fadeOut('slow');
+      
+      });
+      $('.porto6').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay6').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay6').fadeOut('slow');
+      
+      });
+      $('.porto7').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay7').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay7').fadeOut('slow');
+      
+      });
+      $('.porto8').hover(
+          function(){   
+           $('this').stop();
+              $('.overlay8').fadeIn('slow');
+      },function(){
+        $('this').stop()
+          $('.overlay8').fadeOut('slow');
+      
+      });
+  
 
 
 
